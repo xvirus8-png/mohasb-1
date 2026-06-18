1d4eee09cd2e680c4d4de5735b9e92d805ff05c1
mahmood <mahmood@email.com>
2026-06-19 01:35:13 +0300

    v3.6 - بطاقات, Sidebar, PDF, تحديد, فواصل الآلاف, حذف AI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="muhasibi-v3-4";
+const CACHE="muhasibi-v3-6";
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["/"])))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{const r=e.request;if(r.mode==="navigate"||r.headers.get("Accept").includes("text/html")){e.respondWith(fetch(r).catch(()=>caches.match(r)));return}e.respondWith(caches.match(r).then(c=>c||fetch(r).then(res=>{const c2=res.clone();caches.open(CACHE).then(ca=>ca.put(r,c2));return res})))});
